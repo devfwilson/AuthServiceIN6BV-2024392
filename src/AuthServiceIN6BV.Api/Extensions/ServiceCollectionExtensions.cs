e94eaa2f30d728aa75438c5dff60217739a08fd5
wflorian <wflorian-2024392>
2026-02-02 17:16:54 -0600

    02/02/2026

---
+++ src/AuthServiceIN6BV.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+using AuthServiceIN6BV.Application.Services;
+using AuthServiceIN6BV.Application.Interfaces;
+using AuthServiceIN6BV.Domain.Interfaces;
+using AuthServiceIN6BV.Persistence.Data;
+using AuthServiceIN6BV.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+ 
+namespace AuthServiceIN6BV.Api.Extensions;
+ 
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                .UseSnakeCaseNamingConvention());
+ 
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRoleRepository, RoleRepository>();
+
+        services.AddHealthChecks();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApiDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        
+        return services;
+    }
+}
--- src/AuthServiceIN6BV.Domain/obj/Debug/net8.0/AuthServiceIN6BV.Domain.AssemblyInfo.cs
+++ src/AuthServiceIN6BV.Domain/obj/Debug/net8.0/AuthServiceIN6BV.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServiceIN6BV.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2da3f4c8e278d9811bad7df3219890f762b601")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78058c58d83a8f7b3c3a67db550ef790d43c64f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServiceIN6BV.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServiceIN6BV.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
